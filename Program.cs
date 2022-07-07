634d2bba9461fa0ee47a2fa687a3dacf16b4b55e
oleg1 <oleglapshin1987@gmail.com>
2022-07-07 20:28:15 +0400

    mission 2 complete

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿// task 10
+// Console.WriteLine("Input three - digit number:");
+// int number=Convert.ToInt32(Console.ReadLine());
+// int numberTwo (int num) {
+//    int digitTwo=num/10%10;
+//    int digitFour=num/1000%10;
+//    int digitThree=num/100%10;
+//    if (digitFour>0 | digitThree==0){
+//    Console.WriteLine("The number is not range  100...999");
+//    digitTwo=0;
+//    } 
+//       return digitTwo;
+// }
+// Console.WriteLine ("Second digit = "+ numberTwo(number));
+
+// Task 13
+// Console.WriteLine("Input number:");
+// int num=Convert.ToInt32(Console.ReadLine());
+
+// int function (int number)
+// {
+// int digitQuantity=-1;
+// int digitMax=1;
+// int index1=1;
+// while (digitMax>0)
+// {
+//    digitQuantity=digitQuantity+1;
+//    digitMax=number/index1%10;
+//    index1=index1*10;
+// }
+// digitQuantity=digitQuantity-2;
+
+// int devider=1;
+// int index2=0;
+// int digitThree=0;
+// while (index2<digitQuantity)
+// {
+//     digitThree=number/devider%10;
+//     devider=devider*10;
+//     index2=index2+1;     
+// }
+// return digitThree;
+// }
+
+// if (num<100 | num>1000000)
+// {
+//     Console.WriteLine("The number is not range 100...1000000");
+
+    
+// }
+// else
+// {
+//     Console.WriteLine("digitThree= "+function(num));
+// }
+
+// Task 15
+// Console.WriteLine("Input number a weed (1...7):");
+// int number=Convert.ToInt32(Console.ReadLine());
+//     if (number>7 | number<1) 
+//     {
+//         Console.WriteLine("The number is not range 1...7");
+//     }
+//     else if (number==6 | number==7)
+//     {
+//         Console.WriteLine("Ouu, weekend )))");
+//     }
+//     else 
+
+//     Console.WriteLine("weekday (((");
